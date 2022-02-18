fe898c6356e4505b31f4f6649739e69c35b02836
Salomao Soares <salomaosoaresap@gmail.com>
2022-02-18 18:38:00 -0300

    atualizando script aula UC8

---
+++ index.js
@@ -0,0 +1,10 @@
+var listaDePecas = ["Amortecedor","Motor","Filtro de Ar"];
+
+let peso = 50;
+
+if (peso < 100){
+    console.log("A peça deve pesar no mínimo 100g");
+}else{
+    console.log("A peça tem o peso adequado"); 
+}
+

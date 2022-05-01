39f7c44c1f8bbbf8436e0fa5bcd093051557ac18
Salomao Soares <salomaosoaresap@gmail.com>
2022-05-01 10:41:36 -0300

    Pasta de Atividades UC9 - 01-05-2022

--- atividades/Atividades_UC9/EncontrosRemotos/js/script.js
+++ atividades/Atividades_UC9/EncontrosRemotos/js/script.js
@@ -78,6 +78,91 @@ let listaPromocao = [
   }
 ]
 
+let listaNoticias = [
+  {
+    codigo: 1,
+    titulo: "Ranking revela celulares 5G mais populares do mundo; veja top 10",
+    descricao: "Intel Core i3-9100 Box LGA 1151 4.2Ghz 6MB",
+    data: "01/01/2022",
+    imagem: "img/noticia01.webp",
+    link:"https://www.techtudo.com.br/noticias/2022/04/samsung-domina-venda-de-celular-5g-no-planeta-veja-o-ranking.ghtml",
+  },
+  {
+    codigo: 2,
+    titulo: "Sete eletrônicos que falham justamente quando precisamos deles",
+    descricao: "Sete eletrônicos que falham",
+    data: "01/01/2022",
+    imagem: "img/noticia02.webp",
+    link:"https://www.techtudo.com.br/listas/2022/04/sete-eletronicos-que-falham-justamente-quando-precisamos-deles.ghtml",
+  },
+  {
+    codigo: 3,
+    titulo: "Como desaparecer do WhatsApp sem desinstalar o app? Veja 5 maneiras",
+    descricao: "Como desaparecer do WhatsApp",
+    data: "01/01/2022",
+    imagem: "img/noticia03.webp",
+    link:"https://www.techtudo.com.br/listas/2022/04/como-desaparecer-do-whatsapp-sem-desinstalar-o-app-veja-5-maneiras.ghtml",
+  },
+  {
+    codigo: 4,
+    titulo: "Veja 10 jogos nostálgicos que fizeram sucesso e merecem remake",
+    descricao: "Veja 10 jogos nostálgicos que fizeram sucesso e merecem remake",
+    data: "01/01/2022",
+    imagem: "img/noticia04.webp",
+    link:"https://www.techtudo.com.br/listas/2022/04/10-jogos-nostalgicos-que-fizeram-sucesso-e-merecem-remake.ghtml",
+  },
+  {
+    codigo: 5,
+    titulo: "Calculadora informa quantidade de carne necessária para churrasco; saiba usar",
+    descricao: "Calculadora informa quantidade",
+    data: "01/01/2022",
+    imagem: "img/noticia05.webp",
+    link:"https://www.techtudo.com.br/dicas-e-tutoriais/2022/04/como-calcular-carne-para-churrasco-site-faz-calculo-automaticamente.ghtml",
+  },
+  {
+    codigo: 6,
+    titulo: "Conheça os itens essenciais para gravar um podcast",
+    descricao: "Conheça os itens essenciais para gravar um podcast",
+    data: "01/01/2022",
+    imagem: "img/noticia06.webp",
+    link:"https://www.techtudo.com.br/listas/2022/04/5-eletronicos-e-acessorios-essenciais-para-gravar-um-podcast.ghtml",
+  }
+]
+
+let listaCarousel = [
+  {
+    codigo: 1,
+    titulo: "Conheça os itens essenciais para gravar um podcast",
+    descricao: "Conheça os itens essenciais para gravar um podcast",
+    imagem: "img/banner_pilotos.png",
+    link:"https://www.techtudo.com.br/listas/2022/04/5-eletronicos-e-acessorios-essenciais-para-gravar-um-podcast.ghtml",
+  },
+  {
+    codigo: 2,
+    titulo: "Conheça os itens essenciais para gravar um podcast",
+    descricao: "Conheça os itens essenciais para gravar um podcast",
+    imagem: "img/banner_lol.png",
+    link:"https://www.techtudo.com.br/listas/2022/04/5-eletronicos-e-acessorios-essenciais-para-gravar-um-podcast.ghtml",
+  },
+  {
+    codigo: 3,
+    titulo: "Conheça os itens essenciais para gravar um podcast",
+    descricao: "Conheça os itens essenciais para gravar um podcast",
+    imagem: "img/banner_cs.png",
+    link:"https://www.techtudo.com.br/listas/2022/04/5-eletronicos-e-acessorios-essenciais-para-gravar-um-podcast.ghtml",
+  },
+  {
+    codigo: 4,
+    titulo: "Conheça os itens essenciais para gravar um podcast",
+    descricao: "Conheça os itens essenciais para gravar um podcast",
+    imagem: "img/freefire.jpg",
+    link:"https://www.techtudo.com.br/listas/2022/04/5-eletronicos-e-acessorios-essenciais-para-gravar-um-podcast.ghtml",
+  }
+
+]
+
+
+
 function renderizarPromocoes() {
   let espaco = document.getElementById("espaco-promocoes")
 
@@ -114,6 +199,54 @@ function renderizarPromocoes() {
 
 }
 
+function renderizarNoticias() {
+  let espaco = document.getElementById("lista-noticias")
+
+  let template = "";
+
+  for (let index = 0; index < listaNoticias.length; index++) {
+    const noticia = listaNoticias[index];
+
+    template += `
+    <article class="card-noticia">
+    <div class="noticia">
+      <figure class="img-noticia"> 
+        <img src=${noticia.imagem} alt=${noticia.descricao}>
+      </figure> 
+      <div class="titulo-noticia">
+        <h3>
+          <a href=${noticia.link}>${noticia.descricao}</a>
+        </h3>
+        <div class="data-noticia">
+          ${noticia.data}
+        </div>
+      </div>
+    </div>  
+  </article>
+  `;
+  }
+
+  espaco.innerHTML = template;
+
+}
+
+function renderizarCarousel() {
+  let espaco = document.getElementById("carousel-inner")
+  let template = `
+    <div class="carousel-item active">
+    <img class="d-block w-100" src=${listaCarousel[0].imagem} alt=${listaCarousel[0].descricao}>
+    </div>
+      `;
+  for (let index = 1; index < listaCarousel.length; index++) {
+    const lista = listaCarousel[index];
+    template += `
+     <div class="carousel-item">
+      <img class="d-block w-100" src=${lista.imagem} alt=${lista.descricao}>
+     </div>
+     `;
+  }
+  espaco.innerHTML = template;
+}
 
 let menu = document.getElementById("menu")
 
@@ -133,45 +266,12 @@ let email = document.getElementById("campo-email")
 
 function enviarEmail() {
   let emailDigitado = email.value
-  console.log(emailDigitado)
+  alert("Seu email foi enviado com sucesso!");
 }
 
 
 function renderizar(){
+  renderizarCarousel();
   renderizarPromocoes();
-}
-
-
-function renderizarNoticias() {
-  let espaco = document.getElementById("lista-noticias")
-
-  let template = "";
-
-  for (let index = 0; index < listaNoticias.length; index++) {
-    const promocao = listaNoticias[index];
-
-    template += `
-    <div class="cardnews">
-     <div>
-      <img src=${promocao.imagem} alt=${promocao.desc} <br> 
-     </div>
-     <div>
-      <h3>${promocao.titulo}</h3> 
-     </div>
-     <div>
-      <p>${promocao.descricao}</p> 
-     </div>
-     <div>
-      <h1>${promocao.valor}</h1>
-     </div>
-     <div class="bt-comprar"> 
-     <button type="button" class="btn-comprar">COMPRAR</button>
-     </div>
-    </div >
-    `
-
-  }
-
-  espaco.innerHTML = template;
-
+  renderizarNoticias();
 }
